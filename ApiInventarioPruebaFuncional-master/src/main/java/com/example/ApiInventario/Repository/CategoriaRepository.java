ec5e182acedeafa89b70f2d7866b871ae5436f87
Gabriel Duran <gabrieldurans999@gmail.com>
2025-06-27 09:39:08 -0400

    ApiNotificaciones con hateoas y swagger (ApiTerminada)

--- ApiInventarioPruebaFuncional-master/src/main/java/com/example/ApiInventario/Repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ApiInventario.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ApiInventario.Model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
-
-}
